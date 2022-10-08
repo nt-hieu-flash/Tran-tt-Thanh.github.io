837485ef43991c1926fffd6ea28b645b6d8f70b2
nt-hieu-flash <82208113+nt-hieu-flash@users.noreply.github.com>
2022-10-08 13:08:49 +0700

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,40 @@
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlidesModal(slideIndex);
+
+// Next/previous controls
+function plusSlidesModal(n) {
+    showSlidesModal(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlidesModal(slideIndex = n);
+}
+
+function showSlidesModal(n) {
+    var i;
+    var slides = document.getElementsByClassName("modalSlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
